8f6dc87dfca1ebf00e64a0a4c223380ae4393e81
JoMagistelo <josluigomez@gmail.com>
2026-09-03 12:13:46 -0600

    Se añade parser mercado pago

--- tests/readers/test_tesseract_pdf_reader.py
+++ tests/readers/test_tesseract_pdf_reader.py
@@ -7,7 +7,7 @@ from readers.tesseract_pdf_reader import TesseractPDFReader
 
 
 def test_spatial_extraction():
-    ruta_pdf = "data/12. Debito HSBC_JUN26_CAGC940707MDFHRL03.pdf"
+    ruta_pdf = "data/12.3_Inversión_Nafin 9850_dic_23_BALM890924MPLZPR00.pdf"
 
     print("\n========================================================")
     print(" 🛠️ TEST TESSERACT PDF READER (OCR)")
